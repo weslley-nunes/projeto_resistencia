4e2c18093fb833193ab4315a3cd230214f9950d1
weslley-nunes <130309485+weslley-nunes@users.noreply.github.com>
2026-01-12 20:51:04 -0300

    Fix: Add SessionProvider to fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${outfit.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
